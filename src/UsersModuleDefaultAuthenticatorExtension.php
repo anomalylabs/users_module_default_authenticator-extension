5a1bbb602a03157c885b871e1546fa59fbb2f0ca
Ryan Thompson <service@aiwebsystems.com>
2014-10-31 13:27:29 -0500

    Yay

--- src/UsersModuleDefaultAuthenticatorExtension.php
+++ src/UsersModuleDefaultAuthenticatorExtension.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\Streams\Addon\Extension\UsersModuleDefaultAuthenticator;
 
 use Anomaly\Streams\Addon\Module\Users\Extension\AuthenticatorInterface;
+use Anomaly\Streams\Addon\Module\Users\User\Contract\UserRepositoryInterface;
 use Anomaly\Streams\Platform\Addon\Extension\ExtensionAddon;
 
 /**
@@ -17,6 +18,23 @@ class UsersModuleDefaultAuthenticatorExtension extends ExtensionAddon implements
 {
 
     /**
+     * The user repository interface.
+     *
+     * @var
+     */
+    protected $users;
+
+    /**
+     * Create a new UsersModuleDefaultAuthenticatorExtension instance.
+     *
+     * @param UserRepositoryInterface $users
+     */
+    function __construct(UserRepositoryInterface $users)
+    {
+        $this->users = $users;
+    }
+
+    /**
      * Authenticate the credentials.
      *
      * @param array $credentials
@@ -24,9 +42,7 @@ class UsersModuleDefaultAuthenticatorExtension extends ExtensionAddon implements
      */
     public function authenticate(array $credentials)
     {
-        $repository = app('Anomaly\Streams\Addon\Module\Users\User\Contract\UserRepositoryInterface');
-
-        return $repository->findUserByLoginAndPassword($credentials['login'], $credentials['password']);
+        return $this->users->findUserByLoginAndPassword($credentials['login'], $credentials['password']);
     }
 }
  
