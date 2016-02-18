04f8b3b02cdd6ca42ed0e982b1d2da8f5b44d6d1
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 00:27:24 -0600

    Author

--- src/DefaultAuthenticatorExtension.php
+++ src/DefaultAuthenticatorExtension.php
@@ -10,9 +10,9 @@ use Anomaly\UsersModule\User\Contract\UserInterface;
  * Authenticator extensions should return a handler class
  * to do their dirty work.
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\Streams\Addon\Extension\UsersModuleDefaultAuthenticator
  */
 class DefaultAuthenticatorExtension extends AuthenticatorExtension
